d54e1af4962a540b8535f2b09f94278149d8f824
fhkyn <152553424+fhkyn@users.noreply.github.com>
2025-08-19 10:39:38 -0400

    page 2

--- void/index.php
+++ void/index.php
@@ -13,7 +13,6 @@
 	<div class="flex-container">
 		<div class="myheader">
 			<h1 id="myHeader">Verse of the day</h1>
-			<img class="aiart" src="/images/koh01.jfif" alt="Kingdom of Heaven">
 			<br />
 				<h3>
 					“He said to them, “Go into all the world and preach the gospel to all creation. Whoever believes and is baptized will be saved, but whoever does not believe will be condemned.”
@@ -79,8 +78,7 @@
 
 					   
 			<div>
-			<img class="aiart" src="/images/koh01.jfif" alt="Kingdom of Heaven">
-			<h1>A Verse for today </h1>
+						<h1>A Verse for today </h1>
 				<p>
 					<?php
 						$filename2= "file.txt";
