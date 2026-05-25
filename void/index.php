3cf62b01cb768b1ff507f98b2bdbee30d46a1d69
fdhaitech <fdhai.tech@outlook.com>
2026-05-25 15:25:00 -0400

    update 1

--- void/index.php
+++ void/index.php
@@ -124,7 +124,7 @@ if (isset($_POST["paste1"])) {
 ?>
 		<hr />
 			<div class="item4 main">
-					<div class="item1">
+					<div class="item2">
 
 					<p >"If I speak in the tongues of men or of angels, but do not have love, I am only a resounding gong or a clanging cymbal. If I have the gift of prophecy and can fathom all mysteries and all knowledge, and if I have a faith that can move mountains, but do not have love, I am nothing. Love is patient, love is kind. It does not envy, it does not boast, it is not proud. For now we see only a reflection as in a mirror; then we shall see face to face. Now I know in part; then I shall know fully, even as I am fully known. And now these three remain: faith, hope and love. But the greatest of these is love."
 					1 Corinthians 13:1-2, 4, 12-13 NIV</p>
@@ -142,19 +142,19 @@ if (isset($_POST["paste1"])) {
 				  </div>
 				</div>
 				<div>
-				  <div id="p4" class="item1">
+				  <div id="p4" class="item2">
 					<p >Therefore, when Christ came into the world, he said: Sacrifice and offering you did not desire, but a body you prepared for me; Then I said, Here I am - it is written about me in the scroll- I have come to do your will, my God. Let us hold unswervingly to the hope we profess, for he who promised is faithful. But we do not belong to those who shrink back and are destroyed, but to those who have faith and are saved.
 					Hebrews 10:5, 7, 23, 39 NIV</p>
 				
 				  </div>
 
-				  <div class="item1">
+				  <div class="item2">
 					<p >Finally, all of you, be like-minded, be sympathetic, love one another, be compassionate and humble. But even if you should suffer for what is right, you are blessed. Do not fear their threats; do not be frightened.But in your hearts revere Christ as Lord. Always be prepared to give an answer to everyone who asks you to give the reason for the hope that you have. But do this with gentleness and respect.
 					1 Peter 3:8, 14-15 NIV</p>
 				
 				  </div>
 
-				  <div class="item1">
+				  <div class="item2">
 					<p >Be strong and courageous. Do not be afraid or terrified because of them, for the Lord your God goes with you; he will never leave you nor forsake you. The Lord himself goes before you and will be with you; he will never leave you nor forsake you. Do not be afraid; do not be discouraged. Then the Lord appeared at the tent in a pillar of cloud, and the cloud stood over the entrance to the tent.
 					Deuteronomy 31:6, 8, 15 NIV</p>
 				
